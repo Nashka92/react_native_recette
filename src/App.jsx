cf6d4e79674722f09cba85089065b10849843f52
Anna RAACH <annagraph92@gmail.com>
2024-06-14 15:54:52 +0200

    feat(tp_final): confifure navigator and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { Text } from "react-native";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AllCategories from './screen/AllCategories';
+import Meals from './screen/Meals';
+import AboutTheMeal from './screen/AboutTheMeal';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-    return ( 
-        <>
-        <Text>Hello world</Text>
-        </>
-     );
-}
- 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="All Categories">
+        <Stack.Screen name="All Categories" component={AllCategories} />
+        <Stack.Screen name="MealsOverview" component={Meals} />
+        <Stack.Screen name="About the meal" component={AboutTheMeal} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
 export default App;
